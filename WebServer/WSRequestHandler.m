fd1b14b445719a9ff98f8001c086e956b1990392
John Scott <john@jjrscott.com>
2021-02-20 13:12:50 +0000

    Fixed status code string

--- WebServer/WSRequestHandler.m
+++ WebServer/WSRequestHandler.m
@@ -112,7 +112,7 @@
 }
 
 - (void)didReceiveResponseWithURL:(NSURL *)url statusCode:(NSInteger)statusCode headerFields:(nullable NSDictionary<NSString *, NSString *> *)headerFields {
-    [self _sendStringWithFormat:@"HTTP/1.0 %ld %@\r\n", statusCode, [NSHTTPURLResponse localizedStringForStatusCode:statusCode]];
+    [self _sendStringWithFormat:@"HTTP/1.0 %ld %@\r\n", statusCode, [self stringForStatusCode:statusCode]];
     [headerFields enumerateKeysAndObjectsUsingBlock:^(NSString *  _Nonnull key, NSString *  _Nonnull value, BOOL * _Nonnull stop) {
         [self _sendStringWithFormat:@"%@: %@\r\n", key, value];
     }];
@@ -132,6 +132,66 @@
     [data enumerateByteRangesUsingBlock:^(const void * _Nonnull bytes, NSRange byteRange, BOOL * _Nonnull stop) {
         send (self.socketDescriptor, bytes, byteRange.length, 0);
     }];
+}
+
+- (NSString*)stringForStatusCode:(NSInteger)statusCode {
+    // https://github.com/erunion/http-status-code-definitions
+    switch (statusCode) {
+        case 100: return @"Continue";
+        case 101: return @"Switching Protocols";
+        case 102: return @"Processing";
+
+        case 200: return @"OK";
+        case 201: return @"Created";
+        case 202: return @"Accepted";
+        case 203: return @"Non-Authoritative Information";
+        case 204: return @"No Content";
+        case 205: return @"Reset Content";
+        case 206: return @"Partial Content";
+        case 207: return @"Multi-Status";
+
+        case 300: return @"Multiple Choices";
+        case 301: return @"Moved Permanently";
+        case 302: return @"Found";
+        case 303: return @"See Other";
+        case 304: return @"Not Modified";
+        case 305: return @"Use Proxy";
+        case 307: return @"Temporary Redirect";
+
+        case 400: return @"Bad Request";
+        case 401: return @"Authorization Required";
+        case 402: return @"Payment Required";
+        case 403: return @"Forbidden";
+        case 404: return @"Not Found";
+        case 405: return @"Method Not Allowed";
+        case 406: return @"Not Acceptable";
+        case 407: return @"Proxy Authentication Required";
+        case 408: return @"Request Time-out";
+        case 409: return @"Conflict";
+        case 410: return @"Gone";
+        case 411: return @"Length Required";
+        case 412: return @"Precondition Failed";
+        case 413: return @"Request Entity Too Large";
+        case 414: return @"Request-URI Too Large";
+        case 415: return @"Unsupported Media Type";
+        case 416: return @"Requested Range Not Satisfiable";
+        case 417: return @"Expectation Failed";
+        case 422: return @"Unprocessable Entity";
+        case 423: return @"Locked";
+        case 424: return @"Failed Dependency";
+        case 426: return @"Upgrade Required";
+
+        case 500: return @"Internal Server Error";
+        case 501: return @"Method Not Implemented";
+        case 502: return @"Bad Gateway";
+        case 503: return @"Service Temporarily Unavailable";
+        case 504: return @"Gateway Time-out";
+        case 505: return @"HTTP Version Not Supported";
+        case 506: return @"Variant Also Negotiates";
+        case 507: return @"Insufficient Storage";
+        case 510: return @"Not Extended";
+    }
+    return nil;
 }
 
 @end
